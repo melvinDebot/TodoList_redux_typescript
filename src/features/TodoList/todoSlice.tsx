feea3187e74fa387ed11d0ced4eb9056ec3f8930
Melvin Debot <melvin@exceenis.com>
2024-03-24 13:36:38 +0100

    add new api

--- src/features/TodoList/todoSlice.tsx
+++ src/features/TodoList/todoSlice.tsx
@@ -19,6 +19,24 @@ export const fetchTasks = createAsyncThunk("todo/fetchTasks", async () => {
   return tasks as Task[];
 });
 
+export const updateTask = createAsyncThunk(
+  "todo/updateTask",
+  async ({ id, ...updateData }: Task) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${id}`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(updateData),
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to update task");
+    }
+    return await response.json();
+  }
+);
+
 export const todoSlice = createSlice({
   name: "todo",
   initialState,
@@ -28,16 +46,23 @@ export const todoSlice = createSlice({
     },
 
     onDelete: (state, action: PayloadAction<number>) => {
-      return state.filter((item) => item.id !== action.payload);
+      const index = state.findIndex((item) => item.id === action.payload);
+      if (index !== -1) {
+        state.splice(index, 1);
+      }
     },
   },
   extraReducers: (builder) => {
-    builder.addCase(
-      fetchTasks.fulfilled,
-      (state, action: PayloadAction<Task[]>) => {
+    builder
+      .addCase(fetchTasks.fulfilled, (state, action: PayloadAction<Task[]>) => {
         return action.payload;
-      },
-    );
+      })
+      .addCase(updateTask.fulfilled, (state, action: PayloadAction<Task>) => {
+        const index = state.findIndex((item) => item.id === action.payload.id);
+        if (index !== -1) {
+          state[index] = action.payload;
+        }
+      });
     // Gérer également les états pending et rejected si nécessaire
   },
 });
